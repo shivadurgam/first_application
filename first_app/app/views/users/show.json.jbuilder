963d81ae2a787c089e8d014321a99ba5ae9901c4
Shiva Krishna <shivakrishnadurgam@gmail.com>
2014-03-06 13:55:35 +0530

    first commit

---
+++ first_app/app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :first_name, :last_name, :age, :place, :created_at, :updated_at
---
+++ first_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'f277a588a06e91576815e958c4a5919d29c2809ae92c2ecf572e579028dd340602cd08efe3c9fa9abfb206941f0b596efa47c4444b82e8baf8d1fbbd76b8bf2c'
